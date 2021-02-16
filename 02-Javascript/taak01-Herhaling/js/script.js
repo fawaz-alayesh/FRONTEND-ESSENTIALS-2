1a081ea5e107a8abecf2a2f646709e4c01c52135
fawaz-alayesh <fawaz.alayesh@student.rocva.nl>
2021-02-16 13:51:12 +0100

    save changes

--- 02-Javascript/taak01-Herhaling/js/script.js
+++ 02-Javascript/taak01-Herhaling/js/script.js
@@ -1,2 +1,18 @@
 let strNaam;
 let boolNaamKlopt;
+strNaam = prompt("Hoe heet je?");
+boolNaamKlopt = confirm("Heet je "+strNaam+"?");
+console.log(strNaam,boolNaamKlopt);
+if(boolNaamKlopt == true){ 
+    alert("Welkom "+strNaam +"!");
+}
+else { 
+    strNaam = prompt("Nog een keer, hoe heet je?");
+    boolNaamKlopt = confirm("Heet je "+strNaam+"?");
+    if(boolNaamKlopt == true){ 
+    alert("Welkom "+strNaam +"!");
+    }
+    else{
+    alert("Ok, evengoed welkom !");   
+    }
+}
