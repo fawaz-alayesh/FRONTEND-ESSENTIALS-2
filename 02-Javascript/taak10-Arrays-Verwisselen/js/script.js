0babdc24dc5e74b9161065ea2944d2e379c167c9
fawaz-alayesh <fawaz.alayesh@student.rocva.nl>
2021-03-16 11:31:57 +0100

    save changes

--- 02-Javascript/taak10-Arrays-Verwisselen/js/script.js
+++ 02-Javascript/taak10-Arrays-Verwisselen/js/script.js
@@ -17,17 +17,33 @@ document.querySelector(".film4-naam").innerHTML = topFilms[3];
 document.querySelector(".film5-ranking").innerHTML = 5;
 document.querySelector(".film5-naam").innerHTML = topFilms[4];
  
-function moveup(){
-document.querySelector(".btn2-up").innerHTML
-let up= topFilms[0];
-topFilms[0]=topFilms[1];
-topFilms[1]=topFilms[0];
+  
+function moveup(nummer){
+    bewaren = topFilms[nummer-1]
+    verwijderen = topFilms.splice(nummer-1, 1);
+    toevoegen = topFilms.splice(nummer-2, 0, bewaren);
+    console.log(topFilms);
+    iets();
 }
-
-
-function movedown(){
-
+function movedown(nummer){
+    bewaren = topFilms[nummer-1]
+    verwijderen = topFilms.splice(nummer-1, 1);
+    toevoegen = topFilms.splice(nummer, 0, bewaren);
+    console.log(topFilms);
+    iets();
 }
 function resetList(){
-    location.reload();
+    document.getElementById("regel1").innerHTML = "The Godfather";    
+    document.getElementById("regel2").innerHTML = "The Shawshank Redemption";   
+    document.getElementById("regel3").innerHTML = "Schindler's List";   
+    document.getElementById("regel4").innerHTML = "Raging Bull";   
+    document.getElementById("regel5").innerHTML = "Casablanca"; 
+      
+}
+function iets(){
+    document.getElementById("regel1").innerHTML = topFilms[0];
+    document.getElementById("regel2").innerHTML = topFilms[1];
+    document.getElementById("regel3").innerHTML = topFilms[2];
+    document.getElementById("regel4").innerHTML = topFilms[3];
+    document.getElementById("regel5").innerHTML = topFilms[4];
 }
