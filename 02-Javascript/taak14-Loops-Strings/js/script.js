900897ba2c2d4c253e10fb71db35c3cde1c4a67d
fawaz-alayesh <fawaz.alayesh@student.rocva.nl>
2021-03-17 10:57:41 +0100

    save

--- 02-Javascript/taak14-Loops-Strings/js/script.js
+++ 02-Javascript/taak14-Loops-Strings/js/script.js
@@ -6,4 +6,17 @@ function outputLetters(str){
 }
 
 var voorbeeldString = "Dit is een array van letters";
-console.log(voorbeeldString.length); // geeft terug: 
+//console.log(voorbeeldString.length); // geeft terug: 
+function ouput(voorbeeldString){
+for (i = voorbeeldString.length; i <= 28; i++){
+}
+}
+console.log(voorbeeldString);
+bevestigen=prompt("Is dit de goede string ?  "+ voorbeeldString);
+if(bevestigen =="ja"){
+    alert("Oke");
+}
+else{
+    alert("false");
+}
+ouput();
